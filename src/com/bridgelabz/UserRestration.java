0cbcf4e3ecd40f0718f34b319045bcc8983f080a
Mahesh <mahesh51138@gmail.com>
2022-02-25 14:49:02 +0530

    added:Validation Of First Name

---
+++ src/com/bridgelabz/UserRestration.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class UserRestration {
+    public static void main(String[] args) {
+        ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
+        validationOf.validateFirstName();
+    }
+}
