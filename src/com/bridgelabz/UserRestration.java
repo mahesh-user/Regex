422655675e28402d3d8cfe5faf401a9be308e63c
Mahesh <mahesh51138@gmail.com>
2022-02-26 12:25:37 +0530

    added:Validation Of Email

--- src/com/bridgelabz/UserRestration.java
+++ src/com/bridgelabz/UserRestration.java
@@ -2,7 +2,9 @@ package com.bridgelabz;
 
 public class UserRestration {
     public static void main(String[] args) {
+
         ValidationOfUserRegistration validationOf = new ValidationOfUserRegistration();
         validationOf.validateFirstName();
+        validationOf.validemail();
     }
 }
--- src/com/bridgelabz/ValidationOfUserRegistration.java
+++ src/com/bridgelabz/ValidationOfUserRegistration.java
@@ -1,6 +1,9 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ValidationOfUserRegistration {
     Scanner scanner = new Scanner(System.in);
@@ -10,4 +13,13 @@ public class ValidationOfUserRegistration {
         String regex = "^[a-z]{5,}|[A-Z][a-z]{2,}$";
         System.out.println(firstName.matches(regex));
     }
+
+
+ public void validemail() {
+     Scanner email = new Scanner(System.in);
+     String gmail = email.next();
+     System.out.println("enter valid gamail");
+     String regex =  "^[A-Za-z0-9-._]{1,}+@+[a-z]{1,}+.+[a-z]$";
+     System.out.println(gmail.matches(regex));
+ }
 }
